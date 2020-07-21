f18e4be919abaa7325de9671a1c0b4da14f296a1
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 17:32:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     element.include? valid_calls
-  if planeteer_calls.include? valid_calls == true
-    planeteer_calls.find do |element|
-    
+  end
 end
